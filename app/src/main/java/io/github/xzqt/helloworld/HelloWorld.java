9bcbb6ae888a0e7677938edd7f3bb17a9ec4643c
1droidbricker <OneDroidBricker@protonmail.com>
2022-03-20 18:02:55 -0300

    remove throws

--- app/src/main/java/io/github/xzqt/helloworld/HelloWorld.java
+++ app/src/main/java/io/github/xzqt/helloworld/HelloWorld.java
@@ -7,7 +7,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class HelloWorld implements IXposedHookLoadPackage {
 
     @Override
-    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) {
         XposedBridge.log(String.format("[Hello World] loading %s", loadPackageParam.packageName));
     }
 }
